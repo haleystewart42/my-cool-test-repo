a18de98be71ab246fc657f70c17d869390d1c938
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2021-03-18 15:36:30 -0500

    Update MainMenu.cs

--- cool awesome project/Assets/code/MainMenu.cs
+++ cool awesome project/Assets/code/MainMenu.cs
@@ -5,13 +5,8 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+    //hi 
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
